87c78f53b4311aa9f205512e8620195dec2b40d0
Mong Cha <info.mongcha@gmail.com>
2023-12-10 00:48:00 +0600

    Prime number

--- prime-number.c
+++ prime-number.c
@@ -59,3 +59,54 @@ int main(int argc, char const *argv[])
 
 //     return 0;
 // }
+
+// Easy ways to print users input and find prime number
+/*
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int start, end;
+
+    // Get user input for the range
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    printf("Prime numbers from %d to %d are:\n", start, end);
+
+    for (int i = start; i <= end; i++)
+    {
+        bool isPrime = true;
+
+        // Check if i is less than or equal to 1 (0 and 1 are not prime)
+        if (i <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            // Check if i is divisible by any number from 2 to the square root of i
+            for (int j = 2; j * j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        // If isPrime is true, i is a prime number, so print it
+        if (isPrime)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
+*/
