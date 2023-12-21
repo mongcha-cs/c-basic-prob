896f688067a60c2ae331ceea16d4ef357aba1921
Mong Cha <info.mongcha@gmail.com>
2023-12-21 23:36:32 +0600

    Reverse of digits

--- practice-program/05-reverse-of-digits.c
+++ practice-program/05-reverse-of-digits.c
@@ -2,7 +2,21 @@
 
 int main(int argc, char const *argv[])
 {
-    
-    
+    int reverseNumber, remainder, temp, sum = 0;
+
+    printf("Enter your Reverse Number: ");
+    scanf("%d", &reverseNumber);
+
+    temp = reverseNumber;
+
+    while (temp != 0)
+    {
+        remainder = temp % 10;
+        sum = sum * 10 + remainder;
+        temp = temp / 10;
+    }
+
+    printf("Reverse number is: %d", sum);
+
     return 0;
 }
