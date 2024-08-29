2b0af6b0bb0437c8b39ea35794b9cf56c47f72fc
Mong Cha <info.mongcha@gmail.com>
2024-08-30 00:09:07 +0600

    Rectangle value area program

---
+++ mini-problems/18-rectangle-area.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    float length, width, area;
+
+    printf("Enter length value: ");
+    scanf("%f", &length);
+
+    printf("Enter width value: ");
+    scanf("%f", &width);
+
+    area = length * width;
+
+    printf("Area of Rectangle value = %.2f\n", area);
+
+    return 0;
+}
