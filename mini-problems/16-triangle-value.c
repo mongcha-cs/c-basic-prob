492c45ccd5337138aca646410bac4df4d7164d16
Mong Cha <info.mongcha@gmail.com>
2024-08-30 00:16:43 +0600

    Triangle value program

--- mini-problems/16-triangle-value.c
+++ mini-problems/16-triangle-value.c
@@ -1,18 +1,19 @@
+
+// Triangle formula A = 1/2 * b * h
+
 #include <stdio.h>
 
 int main()
 {
     float area, base, height;
 
-    printf("Enter Height value: ");
-    scanf("%f", &height);
-
     printf("Enter Base value: ");
     scanf("%f", &base);
 
-    // Triangle formula = A = 1/2 * h * h
-    area = (float)0.5 * height * base;
+    printf("Enter Height value: ");
+    scanf("%f", &height);
 
+    area = (float)0.5 * base * height;
     printf("The Triangle value is: %.2f\n", area);
 
     return 0;
